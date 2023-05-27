a0fc885ea371f01fbf28e303e85788a5cc6352fb
NirinaTanjona <tanjonawork@gmail.com>
2023-05-27 21:02:23 +0300

    add crud.py

---
+++ backend/api/v1/auth.py
@@ -0,0 +1,7 @@
+from datetime import datetime, timedelta
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
